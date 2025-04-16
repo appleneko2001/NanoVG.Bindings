8a64e2da3e65782921dcda8164a3fbe2b954a21e
appleneko2001 <appleneko2001@outlook.com>
2025-04-17 00:52:57 +0900

    Add .NET demo wrapper

---
+++ NanoVG.Demo.DotNet/Win32NvgLibraryLoader.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace NanoVG.Demo.NetFx;
+
+internal class Win32NvgLibraryLoader : ILibraryLoader
+{
+#pragma warning disable CA2101
+#pragma warning disable SYSLIB1054
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    private static extern IntPtr LoadLibrary(string lpFileName);
+    
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool FreeLibrary(IntPtr hModule);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+#pragma warning restore SYSLIB1054
+#pragma warning restore CA2101
+    
+    public IntPtr? Load(string libName)
+    {
+        return LoadLibrary(libName);
+    }
+
+    public IntPtr? GetFunction(IntPtr pLib, string procName)
+    {
+        return GetProcAddress(pLib, procName);
+    }
+
+    public void Free(IntPtr pLib)
+    {
+        if(!FreeLibrary(pLib))
+            Console.WriteLine("Did not finalise native library!");
+    }
+}
