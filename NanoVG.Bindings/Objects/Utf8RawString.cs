94063a2fb5387f494769afd2dd856929a6594d21
appleneko2001 <appleneko2001@outlook.com>
2025-04-20 04:51:04 +0900

    Update Utf8RawString.cs with maximum buffer length options

--- NanoVG.Bindings/Objects/Utf8RawString.cs
+++ NanoVG.Bindings/Objects/Utf8RawString.cs
@@ -11,8 +11,9 @@ public class Utf8RawString : IDisposable
     
     private readonly IntPtr _ptr;
     private readonly IntPtr _end;
+    private readonly int _maxBytesLength;
     
-    public Utf8RawString(string s)
+    public Utf8RawString(string s, int maxBytesLength = -1)
     {
         if (s == null)
         {
@@ -20,30 +21,59 @@ public class Utf8RawString : IDisposable
             _end = IntPtr.Zero;
             return;
         }
-        
-        _ptr = StringToUtf8StringHGlobal(s, out var cb);
+
+        _maxBytesLength = maxBytesLength < 0 ? s.Length : maxBytesLength;
+        _ptr = StringToUtf8StringHGlobal(s, maxBytesLength, out var cb);
         _end = GetUtf8StringEndAddress(_ptr, cb);
     }
+
+    /// <summary>
+    /// Update the buffer string, not recommended to use if you are created this <see cref="Utf8RawString"/> instance by using non-fixed max length buffer.
+    /// </summary>
+    /// <param name="s">new string (if your string is longer than buffer will be trimmed automatically)</param>
+    public void UpdateData(string s)
+    {
+        UpdateData(Encoding.UTF8.GetBytes(s));
+    }
+
+    public void UpdateData(byte[] buf)
+    {
+        UpdateDataPrivate(buf, _ptr, Math.Min(buf.Length, _maxBytesLength));
+    }
+
+    private static void UpdateDataPrivate(string str, IntPtr ptr, int inputLen)
+    {
+        UpdateDataPrivate(Encoding.UTF8.GetBytes(str), ptr, inputLen);
+    }
     
-    public static IntPtr StringToUtf8StringHGlobal(string str, out int bytesCount)
+    private static void UpdateDataPrivate(byte[] buf, IntPtr ptr, int inputLen)
+    {
+        // copy all data to the buffer
+        Marshal.Copy(buf, 0, ptr, inputLen);
+    }
+    
+    public static IntPtr StringToUtf8StringHGlobal(string str, int maxLen, out int bytesCount)
     {
         bytesCount = 0;
         if (str == null)
             return IntPtr.Zero;
 
-        var byteCount = Encoding.UTF8.GetByteCount(str);
+        var inputLen = Encoding.UTF8.GetByteCount(str);
+        var byteCount = maxLen < 0 ? inputLen : maxLen;
+        
         var ptr = Marshal.AllocHGlobal(byteCount + 1);
         if (ptr == IntPtr.Zero)
             return ptr;
+
+        UpdateDataPrivate(str, ptr, inputLen);
         
-        var buffer = Encoding.UTF8.GetBytes(str);
-        Marshal.Copy(buffer, 0, ptr, byteCount);
+        // put null-terminator at the end of buffer
         Marshal.WriteByte(ptr, byteCount, 0);
 
         bytesCount = byteCount;
         return ptr;
     }
-    
+
     public static IntPtr GetUtf8StringEndAddress(IntPtr strPtr, int bytesCount)
     {
         if (strPtr == IntPtr.Zero)
@@ -56,5 +86,33 @@ public class Utf8RawString : IDisposable
     {
         if(_ptr != IntPtr.Zero)
             Marshal.FreeHGlobal(_ptr);
+    }
+
+    public override string ToString()
+    {
+        return ToString(true);
+    }
+
+    public string ToString(bool getBufferData)
+    {
+        return getBufferData ? GetStringFromBuffer() : $"{_maxBytesLength} bytes buffer";
+    }
+
+    protected string GetStringFromBuffer()
+    {
+        var buf = GetBufferDataPrivate();
+        return Encoding.UTF8.GetString(buf, 0, buf.Length);
+    }
+    
+    protected byte[] GetBufferDataPrivate()
+    {
+        var len = _maxBytesLength;
+        var buf = new byte[len];
+        for (var i = 0; i < len; i++)
+        {
+            buf[i] = Marshal.ReadByte(_ptr, i);
+        }
+
+        return buf;
     }
 }
